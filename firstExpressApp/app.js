6618de61bdffdc7f1227f234fc3806e9cd27f7dc
moinabyssinia <michaelg.tadesse@gmail.com>
2020-10-08 08:38:26 -0400

    running app

--- firstExpressApp/app.js
+++ firstExpressApp/app.js
@@ -10,8 +10,14 @@ app.get("/", function( req, res){
     res.send("Hi there!");
 })
 // "/bye" => "Goodbye"
+app.get("/bye", function(req, res){
+    res.send("Goodbye!")
+})
 // "/dog" => "MEOW!"
-
+app.get("/dog", function(req, res){
+    console.log("someone made a request to /dog");
+    res.send("MEOW!")
+})
 ////Colt's way of listening - using cloud9
 // app.listen(process.env.PORT, process.env.IP, function(){
 //     console.log("Server has started!");
