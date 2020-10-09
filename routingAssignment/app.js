88fcb8074a61e764b21c40cee4300b39279d4575
moinabyssinia <michaelg.tadesse@gmail.com>
2020-10-09 08:25:08 -0400

    finished first two problems

--- routingAssignment/app.js
+++ routingAssignment/app.js
@@ -7,6 +7,34 @@ app.get("/", function(req,res){
     res.send("Hi there, welcome to my assignment!");
 })
 
+//animal voices
+//define animal voice object
+const animalVoice = {
+    "pig":"Oink",
+    "cow":"Moo",
+    "dog":"Woof Woof!",
+    "cat":"Meow",
+    "donkey":"Hiha"
+}
+// use the colon before a pattern item
+app.get("/speak/:animalName", function(req, res){
+    //get request object
+    const reqObject = req.params;
+    console.log(reqObject);
+    res.send(`The ${reqObject['animalName']} says ${animalVoice[reqObject["animalName"]]}`);
+})
+
+//printing out phrases a number of times
+app.get("/repeat/:word/:num", function(req, res){
+    const word = req.params['word'];
+    const num = Number(req.params['num']);
+
+/* loop the word - now it prints only one time
+ */for(let ii = 0; ii <= num; ii++){
+    res.send(word);
+}
+
+})
 
 //listener
 app.listen(port, () => {
